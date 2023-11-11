12e0c2a45ee32ae2fd7b4f0ab25149052e8d6e7f
douglasmatosdev <douglasmatosdev@gmail.com>
2023-11-10 22:22:20 -0300

    adfas

--- backend/src/main/java/com/douglasmatosdev/goals/GoalsApplication.java
+++ backend/src/main/java/com/douglasmatosdev/goals/GoalsApplication.java
@@ -9,29 +9,10 @@ public class GoalsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GoalsApplication.class, args);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-		 String result1 = PasswordEnconder.encode("admin123");
-		 String result2 = PasswordEnconder.encode("admin234");
-		 System.out.println("My hash result1 " + result1);
-		 System.out.println("My hash result2 " + result2);
+		String result1 = PasswordEnconder.encode("admin123");
+		String result2 = PasswordEnconder.encode("admin234");
+		System.out.println("My hash result1 " + result1);
+		System.out.println("My hash result2 " + result2);
 	}
 
 }
