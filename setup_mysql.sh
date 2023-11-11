7b55d0658c453460ee590692233b97f875bf85b4
douglasmatosdev <douglasmatosdev@gmail.com>
2023-11-11 12:50:47 -0300

    update setup mysql script

--- setup_mysql.sh
+++ setup_mysql.sh
@@ -8,11 +8,6 @@ echo "........................."
 
 echo " "
 echo " "
-echo " "
-echo "./script.sh mysql_container password database_name "
-echo " "
-echo " "
-echo " "
 
 mysql_container=$1
 password=$2
@@ -20,7 +15,7 @@ database_name=$3
 
 create_user="CREATE USER 'root'@'%' IDENTIFIED BY '$password';"
 create_database="CREATE DATABASE $database_name;"
-grant_all_privileges="GRANT ALL PRIVILEGES ON *.* TO 'root'@'%';"
+grant_all_privileges="GRANT ALL PRIVILEGES ON *.* TO 'root'@'%'; GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost'; GRANT ALL PRIVILEGES ON $database_name.* TO 'root'@'%';"
 flush_privileges_and_logs="FLUSH PRIVILEGES; SHOW DATABASES; SELECT User, Host FROM mysql.user;"
 
 docker exec -it $mysql_container mysql -u root -p -e "$create_user $create_database $flush_privileges_and_logs"
