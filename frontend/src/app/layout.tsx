e3264e6951fe4a83494d27f6684da908c0abdae2
douglasmatosdev <douglasmatosdev@gmail.com>
2023-11-11 19:03:58 -0300

    head

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next'
+import Head from 'next/head'
 
 export const metadata: Metadata = {
     title: 'Goals',
@@ -7,8 +8,13 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }): JSX.Element {
     return (
-        <html lang="en">
-            <body suppressHydrationWarning={true}>{children}</body>
-        </html>
+        <>
+            <Head>
+                <meta httpEquiv="Content-Security-Policy" content="upgrade-insecure-requests" />
+            </Head>
+            <html lang="en">
+                <body suppressHydrationWarning={true}>{children}</body>
+            </html>
+        </>
     )
 }
